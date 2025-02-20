8e7b7668994c772f129ffb2ddc37b5532b088e85
Pramodh Krishna <pramodh380.com>
2025-02-20 11:22:32 +0530

    yayy

--- server/server.js
+++ server/server.js
@@ -12,10 +12,10 @@ app.use(cors({
 }))
 // MySQL Connection Setup
 const db = mysql2.createConnection({
-    host: 'localhost', //ENV
-    user: 'root', //ENV
-    password: 'Pammu10Pammu@10', //ENV
-    database: 'hod_leave', //ENV
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD, 
+    database: process.env.DB_NAME,
     port: 3306,
     multipleStatements: true
 });
